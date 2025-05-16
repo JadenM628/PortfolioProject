50b67558725596b2de3038f9cb381d62960f212e
Jaden M <marquezgaming628@gmail.com>
2025-05-16 12:29:45 -0400

    Corrected In-line Styling Typo

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -44,7 +44,7 @@ export const About = () => {
                         <h3 className="text-xl font-bold mb-4">Frontend</h3>
                         <div className="flex flex-wrap gap-2">
                             {frontendSkills.map((tech, key) => (
-                                <span key={key} className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow=[0_2px_8px_rgba(59,130,246,0.2)] transition">
+                                <span key={key} className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
                                         {tech}
                                 </span>
                             ))}
@@ -55,7 +55,7 @@ export const About = () => {
                         <h3 className="text-xl font-bold mb-4">Backend</h3>
                         <div className="flex flex-wrap gap-2">
                             {backendSkills.map((tech, key) => (
-                                <span key={key} className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow=[0_2px_8px_rgba(59,130,246,0.2)] transition">
+                                <span key={key} className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition">
                                         {tech}
                                 </span>
                             ))}
@@ -85,7 +85,7 @@ export const About = () => {
                                 </li>
                                 
                                 <li>
-                                     Relevant Coursework: Data Structures, Web Development, Mobile Development, Software Engineering, Database Design and Development
+                                     Relevant Coursework: Data Structures, Web Development, Mobile Application Development, Software Engineering, Database Design and Development
                                 </li>
                             </ul>
                 </div>
