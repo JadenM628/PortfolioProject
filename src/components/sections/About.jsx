a32cd0367ca3e236bc6c601f6c3d9ea8837ac399
Jaden M <marquezgaming628@gmail.com>
2025-05-15 21:50:04 -0400

    Create About Me Page and Content

---
+++ src/components/sections/About.jsx
@@ -0,0 +1,17 @@
+export const About = () => {
+
+    return <section id="about" className="min-h-screen flex items-center justify-center py-20">
+        <div className="max-w-3xl mx-auto px-4">
+            <h2 className="text-3xl font-bold mb-8">About Me</h2>
+
+            <div>
+                <p>
+                    I'm a passionate software engineer with a Bachelor of Science Degree in Computer Science from UNC Charlotte.
+                    I have gained a strong foundation of software engineering skills through my education, as well as through creating several projects.
+                    I enjoy breaking down complex problems and creating simple, efficient solutions, and I'm constantly learning new tools and technologies to expand my skill set.
+                    I'm currently looking for entry-level software engineering opportunities where I can grow, contribute, and make a impact.
+                </p>
+            </div>
+        </div>
+    </section>
+};
