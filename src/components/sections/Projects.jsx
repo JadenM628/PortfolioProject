fe70313b8065260fdd16b7a55d66699abc4d114e
Jaden M <marquezgaming628@gmail.com>
2025-06-01 15:43:36 -0400

    Updated Projects Section

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -7,15 +7,16 @@ export const Projects = () => {
                 <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all">
                     <h3 className="text-xl font-bold mb-2">Weather Dashboard</h3>
                     <p className="text-gray-400 mb-4">A responsive weather application that provides real-time weather information based on user-selected locations.
-                        <div className="flex flex-wrap gap-2 mb-4">
+                        <div className="flex flex-wrap gap-3 mb-4">
                             {["JavaScript", "React", "API Integration"].map((tech, key) => (
                                 <span key={key} className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition all">
                                     {tech}
                                 </span>
                             ))}
                         </div>
-                        <div className="flex jusitfy-between items-center">
+                        <div className="flex justify-between items-center">
                             <a href="https://github.com/JadenM628/Weather_Dashboard" className="text-blue-400 hover:text-blue-300 transition-colors my-4">View Github Repo for this Project →</a>
+                            <a href="https://jadenm628.github.io/Weather_Dashboard/" target="_blank" rel="noopener noreferrer" className="text-rose-400  hover:text-rose-300 transition-colors my-4">Live Demo →</a>
                         </div>
                     </p>
                 </div>
@@ -30,7 +31,7 @@ export const Projects = () => {
                                 </span>
                             ))}
                         </div>
-                        <div className="flex jusitfy-between items-center">
+                        <div className="flex justify-between items-center">
                             <a href="https://github.com/JadenM628/PortfolioProject" className="text-blue-400 hover:text-blue-300 transition-colors my-4">View Github Repo for this Project →</a>
                         </div>
                     </p>
